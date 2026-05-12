d27f0c80ce9297745f501cc9992d34ee170396f2
julesca00 <jfreitadossantos44gmail.com>
2026-05-12 15:26:34 -0300

    commitei as classes

---
+++ Lojinha/DAL/ClientesDAL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lojinha.Modelos;
+
+
+public class Class1
+{
+	public Class1()
+	{
+		
+	}
+}
---
+++ Lojinha/DAL/Dados.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lojinha.Modelos;
+
+public class Class1
+{
+	public Class1()
+	{
+        get
+
+        {
+            return "serve = Data Source=DESKTOP-I4U5BN5\\MSSQLSERVER01;Initial Catalog=dblojinha;Integrated Security=True;Encrypt=False;Trust Server Certificate=True"
+
+        }
+    }
+}
